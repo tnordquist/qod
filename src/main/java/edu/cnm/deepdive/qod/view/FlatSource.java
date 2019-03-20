11d522c6239992949526596b150ad9ae651b48a4
Todd A Nordquist <toddnordquist144@gmail.com>
2019-03-20 09:53:39 -0600

    Implemented random quote controller.

---
+++ src/main/java/edu/cnm/deepdive/qod/view/FlatSource.java
@@ -0,0 +1,15 @@
+package edu.cnm.deepdive.qod.view;
+
+import java.util.Date;
+import java.util.UUID;
+
+public interface FlatSource {
+
+   UUID getId();
+
+   Date getCreated();
+
+   String getName();
+
+
+}
